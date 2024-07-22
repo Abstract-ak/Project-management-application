202e9144b77006b4136c59e9daff2828ff3fbd53
Abstract-ak <ash.king0205@gmail.com>
2024-07-22 23:53:14 +0530

    view project details and select the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
@@ -27,6 +28,15 @@ function App() {
     });
   }
 
+  function handleSelectProject(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectID: id,
+      };
+    });
+  }
+
   // onAdd se jo value aaya yaha iska parameter yaha paas kiya
   // function me. fir value set kiya as an object, old project ko rakhte hua.
   function handleAddProject(projectData) {
@@ -46,7 +56,12 @@ function App() {
   }
 
   console.log(projectsState);
-  let content;
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectID
+  );
+
+  let content = <SelectedProject project={selectedProject} />;
 
   if (projectsState.selectedProjectID === null) {
     content = (
@@ -60,6 +75,7 @@ function App() {
       <ProjectSidebar
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
       />
       {/* <NewProject /> */}
 
